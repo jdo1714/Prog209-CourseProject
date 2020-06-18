77d06f09f5369259fc5287e7edd7874508479854
jdo1714 <j.do1714@outlook.com>
2020-06-18 00:56:33 -0700

    finished node class project

--- node-CourseProject/public/javascripts/main.js
+++ node-CourseProject/public/javascripts/main.js
@@ -1,14 +1,18 @@
-//1. - home, lists all your Notes
-//2. - add, lets you add a Note
-// 3. - lets you delete a note.
-
 // define an array to hold our data.  Later this should be stored on the sever
 Spendings = [];
-// save typing time, make up 3 for testing
-Spendings.push(new Spending("Lunch at Restaurant", "Food", 50, 5, 18, 20));
-Spendings.push(new Spending("New sweaters", "Clothing", 100, 3, 21, 20))
-Spendings.push(new Spending("Videogames", "Gaming", 60, 4, 20, 20));
-Spendings.push(new Spending("Snacks", "Food", 10, 5, 5, 20));
+// for the chart page
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 400;
+canvas.height = 400;
+function rectangle(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x,this.y,this.width,this.height);
+};
 
 // Now comes the code that must wait to run until the document is fully loaded
 document.addEventListener("DOMContentLoaded", function (event) {
@@ -16,6 +20,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
     // update the li's
     let listUl = document.getElementById("listUl");
     UpdateDisplay(listUl);
+    //update canvas
+    document.getElementById("chartDiv").appendChild(canvas);
 
     // this will refresh the data each time you navigate back to the Display page
     $(document).on('pagebeforeshow', '#Display', function () {
@@ -37,41 +43,75 @@ document.addEventListener("DOMContentLoaded", function (event) {
         document.getElementById("dateYear").value = "";
     }
     );
+    $(document).on('pagebeforeshow', '#Chart', function() {
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        var foodAmt = 0;
+        var techAmt = 0;
+        var clothAmt = 0;
+        var miscAmt = 0;
+        for(var i = 0; i < Spendings.length; i++)
+        {
+            if(Spendings[i].type == "Food"){
+                foodAmt = foodAmt + Spendings[i].price;
+            }
+            if(Spendings[i].type == "Technology"){
+                techAmt = techAmt + Spendings[i].price;
+            }
+            if(Spendings[i].type == "Clothing"){
+                clothAmt = clothAmt + Spendings[i].price;
+            }
+            if(Spendings[i].type == "Miscellaneous"){
+                miscAmt = miscAmt + Spendings[i].price;
+            }
+        }
+        foodRect = new rectangle(50,foodAmt,"red", 20, 50);
+        techRect = new rectangle(50,techAmt,"blue",90, 50);
+        clothRect = new rectangle(50, clothAmt, "green",160, 50);
+        miscRect = new rectangle(50, miscAmt, "purple", 230, 50);
+    }
+    );
 
     // add a button event for adding new notes on Edit page
     document.getElementById("newItem").addEventListener("click", function () {
         // use constructor, build new object and put it in array
-        Spendings.push( new Spending( document.getElementById("title").value, 
+        let newSpending = new Spending( document.getElementById("title").value, 
         document.getElementById("type").value,
         document.getElementById("price").value, 
         document.getElementById("dateMonth").value,
         document.getElementById("dateDay").value,
-        document.getElementById("dateYear").value ) );
-        // clear textboxes
-        document.getElementById("title").value = ""; 
-        document.getElementById("type").value = ""; 
-        document.getElementById("price").value  = ""; 
-        document.getElementById("dateMonth").value = "";
-        document.getElementById("dateDay").value = "";
-        document.getElementById("dateYear").value = "";
+        document.getElementById("dateYear").value );
+        
+        $.ajax({
+            url: "/AddSpending",
+            type: "POST",
+            data: JSON.stringify(newSpending),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function(result){
+                console.log(result);
+                document.location.href = "index.html#Display";
+            }
+        });
      });
 
      // add a button even for deleting a note on Edit page
-    //  document.getElementById("delete").addEventListener("click", function () {
-    //     let which = document.getElementById("deleteItem").value;
-    //     let found = false;
-    //     for(var i = 0; i < Spendings.length; i++) // find the match
-    //     {
-    //         if(Spendings[i].title === which){
-    //             Spendings.splice(i,1);  // remove object from array
-    //             found = true;
-    //         }
-    //     }
-    //     if(!found){
-    //         document.getElementById("deleteItem").value = "Sorry, could not find";
-    //     }
+     document.getElementById("delete").addEventListener("click", function () {
+        let which = document.getElementById("deleteItem").value;
+        which = which - 1;
+        $.ajax({
+            type: "DELETE",
+                url: "/DeleteSpending/" +which,
+                success: function(result){
+                    console.log(result);
+                    document.location.href = "index.html#Display";
+                },
+                error: function(xhr, textStatus, errrorThrown) {
+                    console.log('error in operation');
+                    alert("server could not delete item")
+                }
+        });
 
-    //  });
+     });
  
 
 });  // end of code that must wait for document to load before running
@@ -88,36 +128,39 @@ function Spending(pTitle, pType, pPrice, pDateMonth, pDateDay, pDateYear) {
 
 // this function is used by Display page to add li's to the listUl on the page
  function UpdateDisplay(whichElement) {
-    whichElement.innerHTML = "";
-    // sort by date (don't know if this is the best way of accomplishing this)
-    Spendings.sort(function(a, b) {
-        if (a.dateYear > b.dateYear)
-        { return +1; }
-        if (a.dateYear < b.dateYear)
-        { return -1; }
-        if (a.dateYear = b.dateYear)
-        {
-            if (a.dateMonth > b.dateMonth)
+    $.get("/getAllSpendings", function(data,status){
+        Spendings = data;
+        whichElement.innerHTML = "";
+        // sort by date (don't know if this is the best way of accomplishing this)
+        Spendings.sort(function(a, b) {
+            if (a.dateYear > b.dateYear)
             { return +1; }
-            if (a.dateMonth < b.dateMonth)
+            if (a.dateYear < b.dateYear)
             { return -1; }
-            if (a.dateMonth = b.dateMonth)
+            if (a.dateYear = b.dateYear)
             {
-                if (a.dateDay > b.dateDay)
+                if (a.dateMonth > b.dateMonth)
                 { return +1; }
-                if (a.dateDay < b.dateDay)
+                if (a.dateMonth < b.dateMonth)
                 { return -1; }
-                if (a.dateDay = b.dateDay)
-                { return 0; }
+                if (a.dateMonth = b.dateMonth)
+                {
+                    if (a.dateDay > b.dateDay)
+                    { return +1; }
+                    if (a.dateDay < b.dateDay)
+                    { return -1; }
+                    if (a.dateDay = b.dateDay)
+                    { return 0; }
+                }
             }
-        }
 
-    });
+        });
     Spendings.forEach(function(item, index) {
         var li = document.createElement('li');
         whichElement.appendChild(li);
-        li.innerHTML=li.innerHTML + item.dateMonth +"/"+ item.dateDay+"/"+ item.dateYear +" - <b>" + item.type + ":</b> " + item.title + ".  " + " $" + item.price;
+        li.innerHTML=li.innerHTML+ (index + 1) + " -- " + item.dateMonth +"/"+ item.dateDay+"/"+ item.dateYear +" - <b>" + item.type + ":</b> " + item.title + ".  " + " $" + item.price;
     }); // build one li for each item in array
- }
+});
+}
 
 
--- node-CourseProject/routes/index.js
+++ node-CourseProject/routes/index.js
@@ -1,10 +1,84 @@
 var express = require('express');
 var router = express.Router();
 
+function Spending(pTitle, pType, pPrice, pDateMonth, pDateDay, pDateYear) {
+  this.title= pTitle;
+  this.type = pType;
+  this.price = pPrice;
+  this.dateMonth = pDateMonth;
+  this.dateDay = pDateDay;
+  this.dateYear = pDateYear;
+}
+
+ServerSpendings = [];
+
+ServerSpendings.push(new Spending("Lunch at Restaurant", "Food", 50, 5, 18, 20));
+ServerSpendings.push(new Spending("New sweaters", "Clothing", 100, 3, 21, 20))
+ServerSpendings.push(new Spending("Videogames", "Technology", 60, 4, 20, 20));
+ServerSpendings.push(new Spending("Snacks", "Food", 10, 5, 5, 20));
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
   res.sendFile("./public/");
+});
+
+// Get all spendings data
+router.get('/getAllSpendings', function(req, res){
+  res.status(200).json(ServerSpendings);
+});
+// Add new spending
+router.post('/AddSpending', function(req, res){
+  const newSpending = req.body;
+  ServerSpendings.push(newSpending);
+  ServerSpendings.sort(function(a, b) {
+    if (a.dateYear > b.dateYear)
+    { return +1; }
+    if (a.dateYear < b.dateYear)
+    { return -1; }
+    if (a.dateYear = b.dateYear)
+    {
+        if (a.dateMonth > b.dateMonth)
+        { return +1; }
+        if (a.dateMonth < b.dateMonth)
+        { return -1; }
+        if (a.dateMonth = b.dateMonth)
+        {
+            if (a.dateDay > b.dateDay)
+            { return +1; }
+            if (a.dateDay < b.dateDay)
+            { return -1; }
+            if (a.dateDay = b.dateDay)
+            { return 0; }
+        }
+    }
+
+});
+  res.status(200).json(newSpending);
+});
+// Delete spending
+router.delete('/DeleteSpending/:Index', (req,res) => {
+  const indexNumber = req.params.Index;
+  console.log(indexNumber);
+  let found = false;
+        for(var i = 0; i < ServerSpendings.length; i++) // find the match
+        {
+          console.log
+            if(i == indexNumber){
+                ServerSpendings.splice(i,1);  // remove object from array
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            console.log("not found");
+            return res.status(500).json({
+              status: "error"
+            });
+        }
+        else {
+          res.send('Spending deleted!');
+        }
 });
 
 module.exports = router;
